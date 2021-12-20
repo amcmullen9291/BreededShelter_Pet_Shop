47ba8e28d29df6469e15b68202a36db0d474bd92
Anthony <amcmullen9291@gmail.com>
2021-12-20 12:54:10 -0500

    scroll to top button

--- src/main/frontend/src/Components/Facility.js
+++ src/main/frontend/src/Components/Facility.js
@@ -1,12 +1,38 @@
-//import React, { useState, useEffect } from 'react';
-import React from 'react';
-//import { connect, useDispatch } from 'react-redux';
-//import { setStadiumList } from '../Actions/StadiumActions';
+import React, {useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 
 function Facility(){
+
+const ScrollButton = () =>{
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300){
+      setVisible(true)
+    }
+    else if (scrolled <= 300){
+      setVisible(false)
+    }
+  };
+
+  const scrollToTop = () =>{
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
+  return (
+    <button id="scrollButton" onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}}>Top</button>
+  );
+}
+
 return (
         <>
     <div id="mainBackGround">
@@ -18,7 +44,7 @@ return (
     <nav class="crumbsArea">
         <center>
             <ol>
-                <li class="crumb"><a href="/breeded-shelter/facility" id="navBar">Our Breeded Shelter</a></li>
+                <li class="crumb"><a href="/breeded-shelter/facility" id="navBar"><i>Our</i> Breeded Shelter</a></li>
                 <li class="crumb"><a href="/breeded-shelter/staff" id="navBar2">Meet Our Staff</a></li>
                 <li class="crumb"><a href="/breeded-shelter/residents" id="navBar3">Our Residents</a></li>
             </ol>
@@ -32,16 +58,17 @@ return (
     <img className="facilityPics" src={`${process.env.PUBLIC_URL}/Facility/boardinghouse-pic1.jpeg`} alt="..." height="" width="" />
     </div>
     <div className="rightArea">
-    <span id="facilityWords"><br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br/>eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span>
+    <span id="facilityWords"><br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do. <br/>Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span>
     </div>
     <center><img id="bigPic" src={`${process.env.PUBLIC_URL}/Facility/boardinghouse-pic3.jpg`} alt="..." height="" width="" /></center>
-    <span>Text text text...</span>
+    <span className="facilityBtmText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</span>
     </div>
         </div>
 
     <div id="mainBottom">
         <div>
         <hr/>
+        <ScrollButton/>
         <center><button id="button1" type="button" disabled ><b>Ｗ ｅ ｌ ｃ ｏ ｍ ｅ</b></button></center>
         </div>
     </div>
