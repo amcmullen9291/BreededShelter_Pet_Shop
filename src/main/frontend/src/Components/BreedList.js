38f94edfea5ee82d21d88420b917f2627049c490
Anthony <amcmullen9291@gmail.com>
2021-12-20 20:11:20 -0500

    safety commit

--- src/main/frontend/src/Components/BreedList.js
+++ src/main/frontend/src/Components/BreedList.js
@@ -1,46 +1,27 @@
 import { connect, useSelector, useDispatch } from 'react-redux';
 import { filteredBreedList } from '../Actions/BreedActions';
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 function BreedList(){
 
-  const FilteredList = useSelector((state) => state.FilteredList);
-  const dispatch = useDispatch();
   const BREED_LIST_URL = "http://localhost:8080/breeded_shelter/residents";
 
-  console.log("Current Residents: ", FilteredList.state)
-  console.log("State Breweries:", FilteredList)
+    const[ Residents, setResidents ] = useState([]);
 
-  const fetchFilteredList = async () => {
-    const response = await axios.get(BREED_LIST_URL)
-    .then(response => {dispatch(filteredBreedList(response))})
-    .catch((error) => {
-      console.log("Error:", error);
-    });
-  };
-
-  useEffect(() => {
-    fetchFilteredList();
-  }, [])
-
-if(Object.keys(FilteredList.Filter).length >0){
-  console.log("DataSet after length check:", FilteredList.Filter.data)
-  var StateList = FilteredList.Filter.data.map(puppy => {
-    const {id, breedName, group, age, residentsName } = puppy;
-  return (<tr key={id}>
-           <td>{breedName}</td>
-           <td className="spacer"></td>
-           <td>{group}</td>
-           <td className="spacer"></td>
-           <td>{residentsName}</td>
-           <td className="spacer"></td>
-           <td>{age}</td>
-       </tr>
-      )
-  }
-  )
-}
+    useEffect(() => {
+        const fetchBreeds = async() => {
+            try{
+                const resp = await fetch(BREED_LIST_URL);
+                const json = resp.json();
+                console.log(json);
+                setResidents(json.results)
+            }catch(e){
+                console.log(e);
+            }
+        };
+        fetchBreeds();
+    }, []);
 
 return (
     <>
@@ -73,7 +54,6 @@ return (
             <td className="spacer"></td>
             <td>Age (weeks)</td>
             </tr>
-        {StateList}
         </body>
     </table>
     </div>
@@ -88,18 +68,4 @@ return (
     </>
     )
 }
-const mapStateToProps = (state) => {
-  return {
-    FilteredList: state.FilteredList
-  }
-
-}
-
-     const mapDispatchToProps = (dispatch) => {
-       return{
-         FilteredList: (FilteredList) => { dispatch({type: 'FILTERED_LIST', FilteredList})}
-       }
-     }
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(BreedList);
+export default BreedList;
