3710b487f0766d690f74791c271e31668ddffd39
Anthony <amcmullen9291@gmail.com>
2021-12-21 10:28:35 -0500

    updated App.js routing

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Welcome from './Components/Welcome';
 import Menu from './Components/Menu';
 import SelectedBreed from './Components/SelectedBreed';
@@ -11,19 +11,16 @@ import Staff from './Components/Staff';
 // add email form for appointments?
 function App() {
   return (
-    <>
     <Router>
-      <Switch>
-          <Route path={"/"} exact component={Welcome}/>
-          <Route path={"/breeded-shelter/main"} exact component={Menu}/>
-          <Route path={"/breeded-shelter/Staff"} exact component={Staff}/>
-          <Route path={"/breeded-shelter/residents"} exact component={BreedList}/>
-          <Route path={"/breeded-shelter/facility"} exact component={Facility}/>
-          <Route path={"/breeded-shelter/residents/:breed/:id"} exact component={SelectedBreed}/>
-          <Redirect to={"/breeded-shelter/main"} />
-        </Switch>
+      <Routes>
+          <Route path="/" element={<Welcome/>} />
+          <Route path="/breeded-shelter/main" element={<Menu />} />
+          <Route path="/breeded-shelter/Staff" element={<Staff />} />
+          <Route path="/breeded-shelter/residents" element={<BreedList />} />
+          <Route path="/breeded-shelter/facility" element={<Facility />} />
+          <Route path="/breeded-shelter/residents/:breed/:id" element={<SelectedBreed />} />
+        </Routes>
     </Router>
-    </>
   );
 }
 
